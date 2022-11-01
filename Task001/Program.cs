48df983b0492366ecdcc81945c5e731b32b898ca
DmitryKhomyakov~ <Khomyakovds@mail.ru>
2022-11-01 13:41:44 +0300

    solving task 2

---
+++ Task001/Program.cs
@@ -0,0 +1,15 @@
+﻿int A = -9;
+int B = -3;
+int max = -3;
+int min = -3;
+
+if (B > A) B = max;
+else max = A;
+
+if (B < A) B = min;
+else min = A;
+
+Console.WriteLine("max = "); 
+Console.WriteLine(max);
+Console.WriteLine("min = ");
+Console.WriteLine(min);
