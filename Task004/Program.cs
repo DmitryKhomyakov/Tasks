040296ce3a4fbd023c3ad44c99bd1b4c1a80446a
DmitryKhomyakov~ <Khomyakovds@mail.ru>
2022-11-02 13:33:40 +0300

    add Task001

---
+++ Task004/Program.cs
@@ -0,0 +1,12 @@
+﻿int number = 5;
+int startnumber = 1;
+int endnumber = 5;
+int count = 0;
+while (startnumber <= endnumber) 
+    { 
+     if (5 % 2 == 0) Console.WriteLine("чётные");
+    
+    else Console.WriteLine("Нечетное");
+    count = count + 1;
+    }
+if (startnumber > endnumber)  Console.WriteLine("четные числа = ");  
